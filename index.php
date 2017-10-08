3f0036f78f0e87b3f5e860a3b2a100630299cd9c
Peter Laptop <Peter.P.Maltby@gmail.com>
2017-10-08 18:27:45 +0100

    Initial HTML test content generated

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-include "head.html";
 ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+
+    <head>
+        <title>Conforming HTML 4.01 Strict Template</title>
+        <link rel="stylesheet" type="text/css" href="CSS/#" />
+    </head>
+
+    <body>
+        <?php
+            require "HTML/Head.html";
+            require "HTML/Professional.html";
+            require "HTML/Projects.html";
+            require "HTML/Contact.html";
+        ?>
+    </body>
+</html>
