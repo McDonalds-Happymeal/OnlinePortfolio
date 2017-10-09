97ac135c06b79ff4aa76264f92f6729e663d1526
Peter Laptop <Peter.P.Maltby@gmail.com>
2017-10-09 09:58:56 +0100

    working on CSS FML

--- index.php
+++ index.php
@@ -8,16 +8,18 @@
 
     <head>
         <title>Conforming HTML 4.01 Strict Template</title>
-        <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="CSS/Main.css" />
     </head>
 
     <body>
+        <div class="page">
         <?php
             require "HTML/Head.html";
             require "HTML/Professional.html";
             require "HTML/Projects.html";
             require "HTML/Contact.html";
         ?>
+        </div>
     </body>
 </html>
